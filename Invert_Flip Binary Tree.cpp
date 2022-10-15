51a4c4934ac7100509035e920869991f986c800a
Saurav Paul <35382323+Saurav-Paul@users.noreply.github.com>
2022-10-15 21:22:10 +0600

    Create Invert_Flip Binary Tree.cpp

---
+++ Invert_Flip Binary Tree.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/invert-binary-tree/submissions/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void traverse(TreeNode* root){
+        
+        if(root == NULL) return ;
+        
+        swap(root->left, root->right) ;
+        
+        traverse(root->left) ;
+        traverse(root->right) ;
+    }
+    
+    TreeNode* invertTree(TreeNode* root) {
+        traverse(root) ;
+        return root ;
+    }
+};
